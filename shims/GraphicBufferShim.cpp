653d6ce3c2dd993ceb4111cb6e73d2a9e43b8017
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 20:45:01 +0900

    Update GraphicBufferShim.cpp

--- shims/GraphicBufferShim.cpp
+++ shims/GraphicBufferShim.cpp
@@ -1,13 +1,18 @@
+#define LOG_TAG "libshim_mt8167compat_gui"
+
 #include <ui/GraphicBuffer.h>
+#include <utils/Log.h>
 #include <string>
+#include <new>
 
-namespace android {
-
+extern "C"
 __attribute__((visibility("default")))
-__attribute__((used))
-GraphicBuffer::GraphicBuffer(uint32_t inWidth, uint32_t inHeight,
-        PixelFormat inFormat, uint32_t inUsage) :
-    GraphicBuffer(inWidth, inHeight, inFormat, inUsage, std::string("<Unknown>"))
-{
-}
+void _ZN7android13GraphicBufferC1Ejjij(
+        android::GraphicBuffer *thiz,
+        uint32_t inWidth,
+        uint32_t inHeight,
+        android::PixelFormat inFormat,
+        uint32_t inUsage) {
+    new (thiz) android::GraphicBuffer(
+            inWidth, inHeight, inFormat, inUsage, std::string("<Unknown>"));
 }
