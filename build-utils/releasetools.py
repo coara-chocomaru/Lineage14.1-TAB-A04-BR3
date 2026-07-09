9f73aa78bfa3cffcf3d42ccaa259d58ea4981f7e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-10 04:51:54 +0900

    Create releasetools.py

---
+++ build-utils/releasetools.py
@@ -0,0 +1,30 @@
+
+import common
+def FullOTA_InstallEnd(info):
+    OTA_InstallEnd(info, info.input_zip)
+
+def IncrementalOTA_InstallEnd(info):
+    OTA_InstallEnd(info, info.target_zip)
+
+def AddImage(info, input_zip, basename, dest):
+    path = "IMAGES/" + basename
+    if path not in input_zip.namelist():
+        print("Warning: %s not found in target_files; skipping" % path)
+        return
+
+    data = input_zip.read(path)
+    common.ZipWriteStr(info.output_zip, basename, data)
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info, input_zip):
+    info.script.Print("Patching firmware images...")
+    AddImage(info, input_zip, "logo.img", "/dev/block/bootdevice/by-name/logo")
+    AddImage(info, input_zip, "lk.img", "/dev/block/bootdevice/by-name/lk")
+    AddImage(info, input_zip, "lk.img", "/dev/block/bootdevice/by-name/lk2")
+    AddImage(info, input_zip, "tee.img", "/dev/block/bootdevice/by-name/tee1")
+    AddImage(info, input_zip, "tee.img", "/dev/block/bootdevice/by-name/tee2")
+    AddImage(info, input_zip, "preloader.img", "/dev/block/mmcblk0boot0")
+    AddImage(info, input_zip, "preloader.img", "/dev/block/mmcblk0boot1")
+    AddImage(info, input_zip, "twrp.img", "/dev/block/bootdevice/by-name/recovery")
+    AddImage(info, input_zip, "factory.img", "/dev/block/bootdevice/by-name/factory")
+    return
