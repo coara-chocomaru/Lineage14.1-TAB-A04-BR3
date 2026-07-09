095292722e9c3c3dd201986f1f1217d73699cb44
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 20:50:29 +0900

    Update CameraMetadataShim.cpp

--- shims/CameraMetadataShim.cpp
+++ shims/CameraMetadataShim.cpp
@@ -1,5 +1,3 @@
-#define LOG_TAG "libshim_mt8167compat_camera"
-
 #include <camera/CameraMetadata.h>
 #include <camera/VendorTagDescriptor.h>
 #include <utils/Log.h>
@@ -27,23 +25,16 @@ status_t _ZN7android14CameraMetadata14getTagFromNameEPKcPKNS_19VendorTagDescript
     }
 
     if (vTags == NULL) {
-        ALOGE("%s: no VendorTagDescriptor available, cannot resolve '%s'",
-                __FUNCTION__, name);
         return NAME_NOT_FOUND;
     }
+
     const char *lastDot = strrchr(name, '.');
     if (lastDot == NULL) {
-        ALOGE("%s: '%s' is not a fully qualified tag name", __FUNCTION__, name);
         return NAME_NOT_FOUND;
     }
 
     String8 section(name, lastDot - name);
     String8 tagName(lastDot + 1);
 
-    status_t res = vTags->lookupTag(tagName, section, tag);
-    if (res != OK) {
-        ALOGE("%s: failed to resolve vendor tag '%s' (section='%s', tag='%s')",
-                __FUNCTION__, name, section.string(), tagName.string());
-    }
-    return res;
+    return vTags->lookupTag(tagName, section, tag);
 }
