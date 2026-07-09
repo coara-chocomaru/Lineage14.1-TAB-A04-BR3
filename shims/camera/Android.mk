bc78ccbad6d1a5c70954df33461a0efde20a3f76
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 17:17:39 +0900

    Update Android.mk

--- shims/camera/Android.mk
+++ shims/camera/Android.mk
@@ -13,7 +13,13 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libcamera_client \
     libcamera_metadata
-LOCAL_CFLAGS := -Wno-unused-parameter -Wno-non-virtual-dtor
+LOCAL_CFLAGS := \
+    -Wno-unused-parameter \
+    -Wno-non-virtual-dtor \
+    -fvisibility=default \
+    -fno-data-sections \
+    -fno-function-sections
+LOCAL_LDFLAGS := -Wl,--no-gc-sections
 LOCAL_CLANG := true
 LOCAL_C_INCLUDES += \
     frameworks/native/include \
@@ -24,6 +30,18 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MULTILIB := both
 include $(BUILD_SHARED_LIBRARY)
+
+SHIM_VERIFY_STAMP := $(intermediates)/shim_verify.stamp
+
+$(SHIM_VERIFY_STAMP): $(LOCAL_BUILT_MODULE)
+	$(hide) echo "[shim-verify] checking exported symbols in $<"
+	$(hide) nm -D $< | grep -q "T _ZN7android13GraphicBufferC1Ejjij" \
+		|| (echo "FATAL: GraphicBuffer ctor symbol not exported as T" && exit 1)
+	$(hide) nm -D $< | grep -q "T _ZN7android14CameraMetadata14getTagFromNameEPKcPKNS_19VendorTagDescriptorEPj" \
+		|| (echo "FATAL: CameraMetadata::getTagFromName symbol not exported as T" && exit 1)
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SHIM_VERIFY_STAMP)
 
 CAM_UTILS_SRC_32 := vendor/sts/a04br3/proprietary/vendor/lib/libcam_utils.so
 CAM_UTILS_SRC_64 := vendor/sts/a04br3/proprietary/vendor/lib64/libcam_utils.so
@@ -36,6 +54,8 @@ $(hide) mkdir -p $(dir $(2))
 $(hide) cp -f $(1) $(2).tmp
 $(hide) chmod u+w $(2).tmp
 $(hide) patchelf --add-needed libshim_mt8167compat.so $(2).tmp
+$(hide) readelf -d $(2).tmp | grep -q libshim_mt8167compat.so \
+	|| (echo "FATAL: patchelf did not add NEEDED entry to $(2)" && exit 1)
 $(hide) mv -f $(2).tmp $(2)
 $(hide) echo "[shim] patched $(2) with libshim_mt8167compat.so"
 endef
@@ -44,10 +64,14 @@ ifneq ($(wildcard $(CAM_UTILS_SRC_32)),)
 $(CAM_UTILS_OUT_32): $(CAM_UTILS_SRC_32) libshim_mt8167compat
 	$(call patch-cam-utils,$(CAM_UTILS_SRC_32),$(CAM_UTILS_OUT_32))
 ALL_DEFAULT_INSTALLED_MODULES += $(CAM_UTILS_OUT_32)
+else
+$(warning [shim] libcam_utils.so 32bit not found at $(CAM_UTILS_SRC_32))
 endif
 
 ifneq ($(wildcard $(CAM_UTILS_SRC_64)),)
 $(CAM_UTILS_OUT_64): $(CAM_UTILS_SRC_64) libshim_mt8167compat
 	$(call patch-cam-utils,$(CAM_UTILS_SRC_64),$(CAM_UTILS_OUT_64))
 ALL_DEFAULT_INSTALLED_MODULES += $(CAM_UTILS_OUT_64)
+else
+$(warning [shim] libcam_utils.so 64bit not found at $(CAM_UTILS_SRC_64))
 endif
