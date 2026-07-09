9a3c15623f487cc28f0341954844d904da18b633
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 22:16:40 +0900

    Update lineage_a04br3.mk

--- lineage_a04br3.mk
+++ lineage_a04br3.mk
@@ -16,11 +16,11 @@ $(call inherit-product, device/sts/a04br3/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a04br3
 PRODUCT_NAME := lineage_a04br3
-PRODUCT_RELEASE_NAME := TAB-A04-BR3
-PRODUCT_BRAND := benesse
-PRODUCT_MODEL := TAB-A04-BR3
-PRODUCT_MANUFACTURER := STS-TOTTORI
-PRODUCT_GMS_CLIENTID_BASE := android-sts-tottori
+PRODUCT_RELEASE_NAME := PixelTouch_3
+PRODUCT_BRAND := google
+PRODUCT_MODEL := PixelTouch_3
+PRODUCT_MANUFACTURER := google
+PRODUCT_GMS_CLIENTID_BASE := android-sts-google
 PRODUCT_LOCALES := ja_JP en_US en_AU
 PRODUCT_DEFAULT_LANGUAGE := ja
 PRODUCT_DEFAULT_REGION   := JP
@@ -32,6 +32,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="TAB-A04-BR3-userdebug 7.1.2 01.06.000 01.06.000 test-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 7.1.2 NJH47F 4146041 release-keys"
 
-BUILD_FINGERPRINT := benesse/TAB-A04-BR3/TAB-A04-BR3:7.1.2/01.06.000/01.06.000:userdebug/test-keys
+BUILD_FINGERPRINT := google/sailfish/sailfish:7.1.2/NJH47F/4146041:user/release-keys
