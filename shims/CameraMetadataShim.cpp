e14882fb60c4d1cdb89bfdf054f7458749023f6c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 20:45:58 +0900

    Update CameraMetadataShim.cpp

--- shims/CameraMetadataShim.cpp
+++ shims/CameraMetadataShim.cpp
@@ -1,19 +1,49 @@
+#define LOG_TAG "libshim_mt8167compat_camera"
+
 #include <camera/CameraMetadata.h>
-#include <system/camera_vendor_tags.h>
+#include <camera/VendorTagDescriptor.h>
+#include <utils/Log.h>
+#include <utils/String8.h>
 #include <utils/StrongPointer.h>
+#include <cstring>
 
-namespace android {
+using namespace android;
 
+extern "C"
 __attribute__((visibility("default")))
-__attribute__((used))
-status_t CameraMetadata::getTagFromName(const char *name,
-        const VendorTagDescriptor* vTags, uint32_t *tag) {
-    if (vTags == NULL) {
-        sp<VendorTagDescriptor> vTagDesc =
-                VendorTagDescriptor::getGlobalVendorTagDescriptor();
-        vTags = vTagDesc.get();
+status_t _ZN7android14CameraMetadata14getTagFromNameEPKcPKNS_19VendorTagDescriptorEPj(
+        const char *name,
+        const VendorTagDescriptor *vTags,
+        uint32_t *tag) {
+
+    if (name == NULL || tag == NULL) {
+        return BAD_VALUE;
     }
-    (void)vTags;
-    return CameraMetadata::getTagFromName(name, tag);
-}
+
+    sp<VendorTagDescriptor> localVTags;
+    if (vTags == NULL) {
+        localVTags = VendorTagDescriptor::getGlobalVendorTagDescriptor();
+        vTags = localVTags.get();
+    }
+
+    if (vTags == NULL) {
+        ALOGE("%s: no VendorTagDescriptor available, cannot resolve '%s'",
+                __FUNCTION__, name);
+        return NAME_NOT_FOUND;
+    }
+    const char *lastDot = strrchr(name, '.');
+    if (lastDot == NULL) {
+        ALOGE("%s: '%s' is not a fully qualified tag name", __FUNCTION__, name);
+        return NAME_NOT_FOUND;
+    }
+
+    String8 section(name, lastDot - name);
+    String8 tagName(lastDot + 1);
+
+    status_t res = vTags->lookupTag(tagName, section, tag);
+    if (res != OK) {
+        ALOGE("%s: failed to resolve vendor tag '%s' (section='%s', tag='%s')",
+                __FUNCTION__, name, section.string(), tagName.string());
+    }
+    return res;
 }
