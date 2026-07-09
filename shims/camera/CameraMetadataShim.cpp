8217c9c604d7ed0925df71241937ad5b5fb88628
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 12:19:17 +0900

    Change LOG_TAG definition for camera metadata shim

--- shims/camera/CameraMetadataShim.cpp
+++ shims/camera/CameraMetadataShim.cpp
@@ -1,10 +1,12 @@
-#define LOG_TAG "libshim_camera_metadata"
+#define LOG_TAG "libshim_mt8167compat_camera"
 
 #include <camera/CameraMetadata.h>
 #include <system/camera_vendor_tags.h>
+#include <utils/StrongPointer.h>
 
 namespace android {
 
+// _ZN7android14CameraMetadata14getTagFromNameEPKcPKNS_19VendorTagDescriptorEPj
 status_t CameraMetadata::getTagFromName(const char *name,
         const VendorTagDescriptor* vTags, uint32_t *tag) {
     if (vTags == NULL) {
@@ -12,6 +14,8 @@ status_t CameraMetadata::getTagFromName(const char *name,
                 VendorTagDescriptor::getGlobalVendorTagDescriptor();
         vTags = vTagDesc.get();
     }
+    (void)vTags;
     return CameraMetadata::getTagFromName(name, tag);
 }
+
 }
