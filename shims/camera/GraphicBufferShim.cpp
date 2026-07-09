079f40be01f4e06fdd95df8047ee105c19440184
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-09 17:18:45 +0900

    Remove LOG_TAG definition and clean up code

--- shims/camera/GraphicBufferShim.cpp
+++ shims/camera/GraphicBufferShim.cpp
@@ -1,15 +1,13 @@
-#define LOG_TAG "libshim_mt8167compat_gui"
-
 #include <ui/GraphicBuffer.h>
 #include <string>
 
 namespace android {
 
-// _ZN7android13GraphicBufferC1Ejjij
+__attribute__((visibility("default")))
+__attribute__((used))
 GraphicBuffer::GraphicBuffer(uint32_t inWidth, uint32_t inHeight,
         PixelFormat inFormat, uint32_t inUsage) :
     GraphicBuffer(inWidth, inHeight, inFormat, inUsage, std::string("<Unknown>"))
 {
 }
-
 }
