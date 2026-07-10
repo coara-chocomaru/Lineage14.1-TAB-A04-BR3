ae7b9906c8362f1dd5bcc1bc2164132c9fa1e014
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-11 01:23:45 +0900

    Update remove.mk

--- remove.mk
+++ remove.mk
@@ -14,25 +14,28 @@ REMOVE_APPS := \
     Eleven \
     Gallery2 \
     Recorder \
-    Trebuchet
+    Trebuchet \
+    Messaging \
+    Exchange2 \
+    BlockedNumberProvider
 
-PRODUCT_PACKAGES := $(filter-out $(REMOVE_APPS), $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out nfc, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Dialer, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Telephony, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out TelephonyProvider, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out TeleService, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out InCallUi, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out CarrierConfig, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out SimToolkit, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Stk, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Mms, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out EmailProvider, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Camera2, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Messaging, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out Exchange2, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out BlockedNumberProvider, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out com.android.email, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out com.android.exchange, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out com.android.camera2, $(PRODUCT_PACKAGES))
-PRODUCT_PACKAGES := $(filter-out com.android.providers.blockednumber, $(PRODUCT_PACKAGES))
+REMOVE_TELEPHONY := \
+    Dialer \
+    Telephony \
+    TelephonyProvider \
+    TeleService \
+    InCallUi \
+    CarrierConfig \
+    SimToolkit \
+    Stk \
+    Mms \
+    EmailProvider \
+    com.android.email \
+    com.android.exchange \
+    com.android.providers.blockednumber
+
+REMOVE_NFC := \
+    Nfc \
+    Tag
+
+PRODUCT_PACKAGES := $(filter-out $(REMOVE_APPS) $(REMOVE_TELEPHONY) $(REMOVE_NFC), $(PRODUCT_PACKAGES))
