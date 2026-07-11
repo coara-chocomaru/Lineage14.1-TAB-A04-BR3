3c4f2308a3f6ee8d0f5e5ad9a10625b134f39ac8
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-11 16:32:25 +0900

    Update remove.mk

--- remove.mk
+++ remove.mk
@@ -39,3 +39,49 @@ REMOVE_NFC := \
     Tag
 
 PRODUCT_PACKAGES := $(filter-out $(REMOVE_APPS) $(REMOVE_TELEPHONY) $(REMOVE_NFC), $(PRODUCT_PACKAGES))
+
+REMOVE_APPS := 
+Calendar 
+Camera2 
+DeskClock 
+Email 
+ExactCalculator 
+Calculator 
+Jelly 
+LatinIME 
+LockClock 
+WallpaperPicker 
+AudioFX 
+Contacts 
+Eleven 
+Gallery2 
+Recorder 
+Trebuchet 
+Messaging 
+Exchange2 
+BlockedNumberProvider
+
+REMOVE_TELEPHONY := 
+Dialer 
+Telephony 
+TelephonyProvider 
+TeleService 
+InCallUi 
+CarrierConfig 
+SimToolkit 
+Stk 
+Mms 
+EmailProvider 
+com.android.email 
+com.android.exchange 
+com.android.providers.blockednumber
+
+REMOVE_NFC := 
+Nfc 
+Tag
+
+PRODUCT_PACKAGES := $(filter-out 
+$(REMOVE_APPS) 
+$(REMOVE_TELEPHONY) 
+$(REMOVE_NFC), 
+$(PRODUCT_PACKAGES))
