94281f1a47dd26690d46b2fe63c4fc03b8909e1b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-12 04:11:28 +0900

    Update cm.mk to remove unwanted packages
    Remove specified apps, telephony, and NFC from product packages.

--- cm.mk
+++ cm.mk
@@ -2,3 +2,31 @@
 $(call inherit-product, device/sts/a04br3/lineage_a04br3.mk)
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 TARGET_LOCALES := ja_JP en_US en_AU
+REMOVE_APPS := \
+    Calendar \
+    Camera2 \
+    DeskClock \
+    Email \
+    ExactCalculator \
+    Calculator \
+    Jelly \
+    LatinIME \
+    LockClock \
+    WallpaperPicker \
+    AudioFX \
+    Contacts \
+    Eleven \
+    Gallery2 \
+    Recorder \
+    Trebuchet \
+    Exchange2 \
+    BlockedNumberProvider
+
+REMOVE_TELEPHONY := \
+    Dialer \
+
+REMOVE_NFC := \
+    Nfc \
+    Tag
+
+PRODUCT_PACKAGES := $(filter-out $(REMOVE_APPS) $(REMOVE_TELEPHONY) $(REMOVE_NFC), $(PRODUCT_PACKAGES))
