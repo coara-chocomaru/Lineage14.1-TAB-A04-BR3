58d3c63f55567849546da70c6e0d8d9c47466fca
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 08:47:12 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,37 +6,6 @@
 #
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := custom_package_filter
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/dummy
-LOCAL_SRC_FILES := empty
-LOCAL_OVERRIDES_PACKAGES := \
-    Calendar \
-    Camera2 \
-    DeskClock \
-    Email \
-    ExactCalculator \
-    Calculator \
-    Jelly \
-    LatinIME \
-    LockClock \
-    WallpaperPicker \
-    AudioFX \
-    Contacts \
-    Eleven \
-    Gallery2 \
-    Recorder \
-    Trebuchet \
-    Messaging \
-    Exchange2 \
-    BlockedNumberProvider \
-    Dialer \
-    SimToolkit \
-    Stk
-include $(BUILD_PREBUILT)
-
 ifeq ($(TARGET_DEVICE),a04br3)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 $(PRODUCT_OUT)/lk.img: $(LOCAL_PATH)/lk.img
