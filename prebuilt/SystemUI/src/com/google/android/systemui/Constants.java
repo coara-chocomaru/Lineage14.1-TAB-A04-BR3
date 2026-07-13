39a005c4c153d86e82d4f9dc34feb17b7f247606
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 16:22:10 +0900

    Delete prebuilt/SystemUI/src/com/google/android/systemui directory

--- prebuilt/SystemUI/src/com/google/android/systemui/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.google.android.systemui;
-
-public final class Constants {
-    public static final String OPA_COMPONENT_NAME = "com.google.android.googlequicksearchbox/com.google.android.voiceinteraction.GsaVoiceInteractionService";
-    public static final String OPA_ENABLED = "OPA_ENABLED";
-    public static final String OPA_ENABLED_SETTING = "systemui.google.opa_enabled";
-    public static final String OPA_ENABLE_ACTION = "com.google.android.systemui.OPA_ENABLED";
-}
